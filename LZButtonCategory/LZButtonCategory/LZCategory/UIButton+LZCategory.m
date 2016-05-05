ef365d78fa6b6fa8457a195ceb2764e31f8b1697
LQQZYY <592852848@qq.com>
2016-05-05 23:09:07 +0800

    no message

--- LZButtonCategory/LZButtonCategory/LZCategory/UIButton+LZCategory.m
+++ LZButtonCategory/LZButtonCategory/LZCategory/UIButton+LZCategory.m
@@ -11,26 +11,21 @@
 @implementation UIButton (LZCategory)
 
 - (void)LZSetbuttonType:(LZCategoryType)lzType {
+    
+    //需要在外部修改标题背景色的时候将此代码注释
+    self.titleLabel.backgroundColor = self.backgroundColor;
+    self.imageView.backgroundColor = self.backgroundColor;
+    
+    CGSize titleSize = self.titleLabel.bounds.size;
+    CGSize imageSize = self.imageView.bounds.size;
+    CGFloat interval = 1.0;
+    
     if (lzType == LZCategoryTypeLeft) {
-        //需要在外部修改标题背景色的时候将此代码注释
-        self.titleLabel.backgroundColor = self.backgroundColor;
-        self.imageView.backgroundColor = self.backgroundColor;
-        
-        CGSize titleSize = self.titleLabel.bounds.size;
-        CGSize imageSize = self.imageView.bounds.size;
-        CGFloat interval = 1.0;
-        
+
         [self setImageEdgeInsets:UIEdgeInsetsMake(0,titleSize.width + interval, 0, -(titleSize.width + interval))];
         [self setTitleEdgeInsets:UIEdgeInsetsMake(0, -(imageSize.width + interval), 0, imageSize.width + interval)];
 
     } else if(lzType == LZCategoryTypeBottom) {
-        //需要在外部修改标题背景色的时候将此代码注释
-        self.titleLabel.backgroundColor = self.backgroundColor;
-        self.imageView.backgroundColor = self.backgroundColor;
-        
-        CGSize titleSize = self.titleLabel.bounds.size;
-        CGSize imageSize = self.imageView.bounds.size;
-        CGFloat interval = 1.0;
         
         [self setImageEdgeInsets:UIEdgeInsetsMake(0,0, titleSize.height + interval, -(titleSize.width))];
         [self setTitleEdgeInsets:UIEdgeInsetsMake(imageSize.height + interval, -(imageSize.width), 0, 0)];
